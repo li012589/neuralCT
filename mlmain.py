e9ffe1539e1447c19329c2ae9ab699cda040a97b
Li SH <li012589li@outlook.com>
2019-09-23 22:18:42 +0800

    update ml task script

--- mlmain.py
+++ mlmain.py
@@ -7,7 +7,6 @@ from torch import nn
 
 import flow
 import source
-import hamilton
 import train
 import utils
 
@@ -17,31 +16,30 @@ import h5py
 #torch.manual_seed(42)
 parser = argparse.ArgumentParser(description='')
 
-group = parser.add_argument_group('learning  parameters')
-parser.add_argument("-folder", default=None,help = "folder to save and load")
-group.add_argument("-Nepochs", type=int, default=1000, help="")
-group.add_argument("-batch", type=int, default=1000, help="")
-group.add_argument("-cuda", type=int, default=-1, help="use GPU")
-group.add_argument("-lr", type=float, default=0.001, help="learning rate")
-group.add_argument("-save", action='store_true',help="if save or not")
-group.add_argument("-load", action='store_true' ,help="if load or not")
-group.add_argument("-seed", type=int, default=-1 ,help="manual_seed or not")
-group.add_argument("-save_period", type=int, default=10, help="")
+group = parser.add_argument_group('Learning  parameters')
+parser.add_argument("-folder", default=None,help = "Folder to save and load")
+group.add_argument("-epochs", type=int, default=400, help="Number of epoches to train")
+group.add_argument("-batch", type=int, default=200, help="Batch size of train")
+group.add_argument("-cuda", type=int, default=-1, help="If use GPU")
+group.add_argument("-lr", type=float, default=0.001, help="Learning rate")
+group.add_argument("-save", action='store_true',help="If save or not")
+group.add_argument("-load", action='store_true' ,help="If load or not")
+group.add_argument("-save_period", type=int, default=10, help="Steps to save in train")
 group.add_argument("-K",type=float, default=300, help="Temperature")
-group.add_argument("-double", action='store_true',help="use double or single")
+group.add_argument("-double", action='store_true',help="Use double or single")
 
-group = parser.add_argument_group('network parameters')
-group.add_argument("-hdim", type=int, default=128, help="")
-group.add_argument("-numFlow", type=int, default=1, help="depth")
-group.add_argument("-nlayers", type=int, default=8, help="depth")
-group.add_argument("-nmlp", type=int, default=2, help="depth")
-group.add_argument("-typeLayer", type=int, default=1, help="")
-group.add_argument("-shift",action="store_false",help="shift or not")
-group.add_argument("-relax",action="store_false",help="relax or not")
 
-group = parser.add_argument_group('target parameters')
-group.add_argument("-n",type=int, default=784,help="number of dimensions")
-group.add_argument("-dataset", default="./mnist.npz", help="path to training data")
+group = parser.add_argument_group('Network parameters')
+group.add_argument("-hdim", type=int, default=680, help="Hidden dimension of mlps")
+group.add_argument("-numFlow", type=int, default=1, help="Number of flows")
+group.add_argument("-nlayers", type=int, default=16, help="Number of mlps in rnvp")
+group.add_argument("-nmlp", type=int, default=3, help="Number of layers of mlps")
+group.add_argument("-shift",action="store_false",help="Shift latent variable or not")
+group.add_argument("-relax",action="store_false",help="Trainable latent p or not")
+
+group = parser.add_argument_group('Target parameters')
+group.add_argument("-n",type=int, default=784,help="Number of dimensions")
+group.add_argument("-dataset", default="./database/mnist.npz", help="Path to training data")
 
 args = parser.parse_args()
 
@@ -64,8 +62,7 @@ if not args.load:
     nmlp = args.nmlp
     lr = args.lr
     batchSize = args.batch
-    Nepochs = args.Nepochs
-    typeLayer = args.typeLayer
+    epochs = args.epochs
     K = args.K
     with h5py.File(rootFolder+"/parameter.hdf5","w") as f:
         f.create_dataset("n",data=n)
@@ -76,8 +73,7 @@ if not args.load:
         f.create_dataset("nmlp",data=nmlp)
         f.create_dataset("lr",data=lr)
         f.create_dataset("batchSize",data=batchSize)
-        f.create_dataset("Nepochs",data=Nepochs)
-        f.create_dataset("typeLayer",data=typeLayer)
+        f.create_dataset("epochs",data=epochs)
         f.create_dataset("K",data=K)
 else:
     with h5py.File(rootFolder+"/parameter.hdf5","r") as f:
@@ -89,35 +85,37 @@ else:
         nmlp = int(np.array(f["nmlp"]))
         lr = int(np.array(f["lr"]))
         batchSize = int(np.array(f["batchSize"]))
-        Nepochs = int(np.array(f["Nepochs"]))
-        typeLayer = int(np.array(f["typeLayer"]))
+        epochs = int(np.array(f["epochs"]))
         K = int(np.array(f["K"]))
 
-from dataloader import MDSampler,load
+from utils import MDSampler,load
 
 loadrange = ["arr_0"]
 dataset = load(args.dataset).to(device)
 
+if not args.double:
+    dataset = dataset.to(torch.float32)
+
 target = MDSampler(dataset)
 
-def innerBuilder(n):
+def innerBuilder(num):
     maskList = []
     for i in range(nlayers):
         if i %2==0:
-            b = torch.zeros(n)
+            b = torch.zeros(num)
             i = torch.randperm(b.numel()).narrow(0, 0, b.numel() // 2)
             b.zero_()[i] = 1
-            b=b.reshape(1,n)
+            b=b.reshape(1,num)
         else:
             b = 1-b
         maskList.append(b)
     maskList = torch.cat(maskList,0).to(torch.float32)
-    fl = flow.RNVP(maskList, [utils.SimpleMLPreshape([n]+[hidden]*nmlp+[n],[nn.Softplus()]*nmlp+[None]) for _ in range(nlayers)], [utils.SimpleMLPreshape([n]+[hidden]*nmlp+[n],[nn.Softplus()]*nmlp+[utils.ScalableTanh(n)]) for _ in range(nlayers)])
+    fl = flow.RNVP(maskList, [utils.SimpleMLPreshape([num]+[hidden]*nmlp+[num],[nn.Softplus()]*nmlp+[None]) for _ in range(nlayers)], [utils.SimpleMLPreshape([num]+[hidden]*nmlp+[num],[nn.Softplus()]*nmlp+[utils.ScalableTanh(num)]) for _ in range(nlayers)])
     return fl
 
-from flowBuilder import flowBuilder2
+from utils import flowBuilder
 
-f = flowBuilder2(n,numFlow,innerBuilder,typeLayer,relax=args.relax,shift=args.shift).to(device)
+f = flowBuilder(n,numFlow,innerBuilder,1,relax=args.relax,shift=args.shift).to(device)
 
 if not args.double:
     f = f.to(torch.float32)
@@ -145,7 +143,7 @@ if args.load:
     import pdb
     pdb.set_trace()
 
-LOSS = train.forwardLearn(target,f,batchSize,Nepochs,lr,saveSteps = lossPlotStep,savePath=rootFolder)
+LOSS = train.forwardLearn(target,f,batchSize,epochs,lr,saveSteps = lossPlotStep,savePath=rootFolder)
 
 import pdb
 pdb.set_trace()
