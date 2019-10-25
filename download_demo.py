eb60efb6bda6e4b4c581b3ef9bd5c8931fe4e67c
Li SH <li012589li@outlook.com>
2019-10-26 03:01:12 +0800

    clear

--- download_demo.py
+++ download_demo.py
@@ -1,8 +1,6 @@
 import os
 import subprocess
 
-#url = "https://drive.google.com/file/d/1vkKrxT599XIQV_tbTXLIxKT9RC5EQohV/view?usp=sharing"
-#url = "https://drive.google.com/uc?export=download&id=1vkKrxT599XIQV_tbTXLIxKT9RC5EQohV"
 url = "https://www.googleapis.com/drive/v3/files/1HeLY9uY0897uSVq9-ve0E7Weo8KgXcj0?alt=media&key=AIzaSyA7V08DpkCyfdWPiJ40Kkcvru5Ay-UWHis"
 cmds =  [["wget",url,"-O","demos.zip"],["unzip","demos.zip"],["mv","files/demo","demo"],["mv","files/database","database"],["rm","-r","files"]]
 
