8a2d408390b4e760059787278a22c442b5d2c046
Li SH <li012589li@outlook.com>
2019-12-20 11:40:32 +0800

    add interp plots

---
+++ testMNIST.py
@@ -0,0 +1,132 @@
+import numpy as np
+import torch
+from torch import nn
+import flow
+import train
+import utils
+import math
+import h5py
+
+# Set gobal variables.
+
+rootFolder = "./demo/MNIST_relax_True_shift_True_T_300_depthLevel_1_l16_M3_H680/"
+device = torch.device("cpu")
+dtype = torch.float32
+dataset = "./database/mnist.npz"
+labdata = "./database/mnistlabs.npz"
+
+# Load paremeters
+
+with h5py.File(rootFolder+"/parameter.hdf5","r") as f:
+    n = int(np.array(f["n"]))
+    numFlow = int(np.array(f["numFlow"]))
+    lossPlotStep = int(np.array(f["lossPlotStep"]))
+    hidden = int(np.array(f["hidden"]))
+    nlayers = int(np.array(f["nlayers"]))
+    nmlp = int(np.array(f["nmlp"]))
+    lr = int(np.array(f["lr"]))
+    batchSize = int(np.array(f["batchSize"]))
+    epochs = int(np.array(f["epochs"]))
+    K = int(np.array(f["K"]))
+    
+# Build the target.
+
+from utils import MDSampler,load
+dataset = load(dataset).to(device).to(dtype)
+datasetlabs = torch.from_numpy(np.argmax(np.load(labdata)["arr_0"],axis=1)).to(device).to(dtype)
+target = MDSampler(dataset)
+    
+# Rebuild the model.
+
+def innerBuilder(num):
+    maskList = []
+    for i in range(nlayers):
+        if i %2==0:
+            b = torch.zeros(num)
+            i = torch.randperm(b.numel()).narrow(0, 0, b.numel() // 2)
+            b.zero_()[i] = 1
+            b=b.reshape(1,num)
+        else:
+            b = 1-b
+        maskList.append(b)
+    maskList = torch.cat(maskList,0).to(torch.float32)
+    fl = flow.RNVP(maskList, [utils.SimpleMLPreshape([num]+[hidden]*nmlp+[num],[nn.Softplus()]*nmlp+[None]) for _ in range(nlayers)], [utils.SimpleMLPreshape([num]+[hidden]*nmlp+[num],[nn.Softplus()]*nmlp+[utils.ScalableTanh(num)]) for _ in range(nlayers)])
+    return fl
+
+from utils import flowBuilder
+
+f = flowBuilder(n,numFlow,innerBuilder,1).to(device).to(dtype)
+
+# Load saving.
+
+import os
+import glob
+name = max(glob.iglob(rootFolder+"savings/"+'*.saving'), key=os.path.getctime)
+print("load saving at "+name)
+saved = torch.load(name,map_location=device)
+f.load(saved);
+
+# Calculate modes in the latent space.
+
+d0 = f.layerList[0].elements[:n]
+d1 = f.layerList[0].elements[n:]
+omega = (1/(torch.exp(d0+d1))).detach()
+omega, idx = torch.sort(omega)
+
+from matplotlib import pyplot as plt
+from utils import logit_back,logit
+
+frnvp = utils.extractFlow(f).to(dtype)
+
+sample1 = target.sample(1)
+sample2 = target.sample(1)
+
+zsample1 = f.forward(sample1)[0][:,:784].detach()
+zsample2 = f.forward(sample2)[0][:,:784].detach()
+
+xoriginal1 = frnvp.inverse(zsample1)[0].detach()
+
+
+plt.figure()
+plt.imshow(logit_back(sample1[:,:784].reshape(28,28)),cmap="gray")
+plt.figure()
+plt.imshow(logit_back(sample2[:,:784].reshape(28,28)),cmap="gray")
+
+
+L = 10.
+
+delta = (zsample2-zsample1)/L
+
+dims = [5,10,20,40,120,200,450,784]
+
+from copy import deepcopy
+BIGSAMPLES = []
+for dim in dims:
+    SAMPLES = []
+    for i in range(int(L+1)):
+        _tmp = deepcopy(zsample1)
+        _dim = idx[:dim]
+        _tmp[:,_dim] = zsample1[:,_dim]+(delta*i)[:,_dim]
+        SAMPLES.append(frnvp.inverse(_tmp)[0].detach().reshape(28,28))
+    imgs = torch.cat(SAMPLES,1)
+    BIGSAMPLES.append(imgs)
+imgs = torch.cat(BIGSAMPLES,0)
+
+plt.figure(figsize=(12,12))
+plt.imshow(logit_back(imgs),cmap="gray")
+
+BIGSAMPLES = []
+for dim in dims:
+    SAMPLES = []
+    for i in range(int(L+1)):
+        _tmp = deepcopy(zsample1)
+        _tmp[:,:dim] = zsample1[:,:dim]+(delta*i)[:,:dim]
+        SAMPLES.append(frnvp.inverse(_tmp)[0].detach().reshape(28,28))
+    imgs = torch.cat(SAMPLES,1)
+    BIGSAMPLES.append(imgs)
+imgs = torch.cat(BIGSAMPLES,0)
+
+plt.figure(figsize=(12,12))
+plt.imshow(logit_back(imgs),cmap="gray")
+plt.show()
+
